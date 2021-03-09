8b3a42f0a1ed4ce36e3b9fd43a7bd888a1710bb3
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-10 02:26:15 +0300

    Added logic for departments model

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,35 @@
 package models;
 
+import dao.DepartmentDao;
+
+import java.util.List;
+import java.util.Objects;
 public class Departments {
+    private String name;
+    private String description;
+    private int user;
+    private int id;
+
+    /*Constructor*/
+    public Departments(String name, String description, int user) {
+        this.name = name;
+        this.description = description;
+        this.user = user;
+    }
+
+    /*Getters*/
+    public String getName() {
+        return  name;
+    };
+    public int getId() {
+        return id;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public int getUser() {
+        return user;
+    }
+
 }

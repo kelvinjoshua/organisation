dfe7f236f103dab082a7bb0142f6a299114b1d89
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-10 10:57:19 +0300

    Added logic to remaining methods in news DAO

--- src/main/java/dao/sql2oNewsDao.java
+++ src/main/java/dao/sql2oNewsDao.java
@@ -21,15 +21,25 @@ public class sql2oNewsDao  implements  NewsDao{
             System.out.println(ex);
         }
     }
-
+/*Fetch object  with this unique id*/
     @Override
     public News findByiId(int id) {
-        return null;
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM news WHERE id = :id").addParameter("id", id).executeAndFetchFirst(News.class);
+        }
     }
 
     @Override
     public List<News> getAllNews() {
-        return null;
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM news").executeAndFetch(News.class);
+        }
+    }
+    /*one to many*/
+    public List<News> getAllNewsByDepartment(int departmentId) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM news WHERE departmentid = :departmentid").addParameter("departmentid", departmentId).executeAndFetch(News.class);
+        }
     }
 
     @Override
@@ -39,11 +49,21 @@ public class sql2oNewsDao  implements  NewsDao{
 
     @Override
     public void deleteById(int id) {
-
+        String sql = "DELETE from news WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).addParameter("id", id).executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
     }
-
     @Override
     public void clearAll() {
-
+        String sql = "DELETE from news";
+        try (Connection con = sql2o.open()) {
+                 con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
+
 }

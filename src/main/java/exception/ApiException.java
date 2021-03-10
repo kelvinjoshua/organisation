b48146aefae4c3db12ca4631c0736a0865cdb5d7
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-10 13:03:32 +0300

    Added logic to ApiException

--- src/main/java/exception/ApiException.java
+++ src/main/java/exception/ApiException.java
@@ -1,4 +1,12 @@
 package exception;
 
 public class ApiException  extends RuntimeException{
+    private final int statusCode;
+    public ApiException (int statusCode, String msg){
+        super(msg);/*reference parent class*/
+        this.statusCode = statusCode;
+    }
+    public int getStatusCode() {
+        return statusCode;
+    }
 }

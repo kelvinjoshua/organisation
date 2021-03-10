efb13837ab406293cae2ccf070081ac557d678b0
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-10 10:20:10 +0300

    Logic for News model

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,7 +1,44 @@
 package models;
 
 public class News {
-    /*Getters*/
+    private int id;
+    private int departmentid;
+    private String content;
+    private String description;
 
+    public News(String content, String description, int departmentid) {
+        this.content = content;
+        this.description = description;
+        this.departmentid = departmentid;
+    }
+    /*Getters*/
+    public String getContent() {
+        return content;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getDepartmentid() {
+        return departmentid;
+    }
+
+    public int getId() {
+        return id;
+    }
     /*Setters-useful for our restful routing*/
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDepartmentid(int departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,4 +1,6 @@
 package models;
 
 public class Users {
+
+
 }

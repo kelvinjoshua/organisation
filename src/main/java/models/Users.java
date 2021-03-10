72eaf2fac05b106ea5eb717003987cbf12a8e7ae
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-10 10:28:56 +0300

    LOgic for user model

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,6 +1,57 @@
 package models;
 
+import java.util.Objects;
+
 public class Users {
+    private int id;
+    private String name;
+    private int departmentid;
+    private String role;
 
+    public Users(String name, int departmentid, String role) {
+        this.name = name;
+        this.departmentid = departmentid;
+        this.role = role;
+    }
+/*getters*/
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getDepartmentId() {
+        return departmentid;
+    }
+    public String getRole() {
+        return role;
+    }
+    /*setters*/
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public void setName(String userName) {
+        this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setDepartmentId(int departmentid) {
+        this.departmentid = departmentid;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Users)) return false;
+        Users user = (Users) o;//type cast
+        return departmentid == user.departmentid &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, departmentid, role);
+    }
 }

8377c889b17fbb21306297076e11f9913b7d76b1
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-11 18:55:08 +0300

    change on DB

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -3,6 +3,6 @@ import org.sql2o.*;
 
 public class DB {
   //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/department_news", "bale","kelvin23");
-    String connectionString ="jdbc:postgresql://ec2-54-159-175-113.compute-1.amazonaws.com:5432/db3f6fp0tt5q0p";
-    Sql2o sql2o = new Sql2o(connectionString, "vfouuxjniurlir","01aebc052cb6f50d8ebb2c321e70f6e168d06e8b43994bb320e5f34b08a7bd43");
+  String connectionString ="jdbc:postgresql://ec2-54-164-22-242.compute-1.amazonaws.com:5432/dbq1sgh4qbl6of";
+  Sql2o sql2o = new Sql2o(connectionString, "zleejguknelkym","22f5f90071136259e8d6804c932d9b2e2c36e299bf1884534418e9be31072739");
 }

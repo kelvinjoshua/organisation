48ceb47035ea572f4d8eaca642626fc67b5aeb8a
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-11 15:48:29 +0300

    Expose heroku port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,9 +25,9 @@ public class App {
               sql2oUserDao usersObj;
               Connection conn;
               Gson gson = new Gson(); /*convert obj to java from json and back*/
-           //String connectionString ="jdbc:postgresql://ec2-54-159-175-113.compute-1.amazonaws.com:5432/db3f6fp0tt5q0p";
-          // Sql2o sql2o = new Sql2o(connectionString, "vfouuxjniurlir","01aebc052cb6f50d8ebb2c321e70f6e168d06e8b43994bb320e5f34b08a7bd43");
-           Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/department_news", "bale","kelvin23");
+           String connectionString ="jdbc:postgresql://ec2-54-159-175-113.compute-1.amazonaws.com:5432/db3f6fp0tt5q0p";
+           Sql2o sql2o = new Sql2o(connectionString, "vfouuxjniurlir","01aebc052cb6f50d8ebb2c321e70f6e168d06e8b43994bb320e5f34b08a7bd43");
+           //Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/department_news", "bale","kelvin23");
 
               DepartmentDao = new sql2oDepartmentDao(sql2o);
               usersObj= new sql2oUserDao(sql2o);
